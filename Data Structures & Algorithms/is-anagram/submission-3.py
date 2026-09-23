e30a904116ae09c68cce2ee13f24bd89c0b95fec
Lam Dao <honglam1320@gmail.com>
2026-09-22 23:30:43 -0600

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        dict = {}
+        for i in range(len(s)):
+            c1 = s[i].lower()
+            if dict.get(c1):
+                dict[c1] += 1
+            else:
+                dict[c1] = 1
+
+            c2 = t[i].lower()
+            if dict.get(c2):
+                dict[c2] -= 1
+            else:
+                dict[c2] = -1
+            
+        result = set(dict.values())
+        return len(result) == 1 and 0 in result
+        
